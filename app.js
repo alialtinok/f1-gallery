6dff325f1e36ec1b892e87f0bc200c3c932a95bf
Ali Altınok <info.alialtinok@gmail.com>
2022-01-24 23:26:02 +0300

    ejs template engine && partial view

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-const express = require('express')
-// const res = require('express/lib/response')
-const app = express()
+const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const app = express();
+const path = require('path');
+const ejs = require('ejs');
 
-const myLogger = (req,res,next)=> {
-  console.log('MiddleWare katmanı çalıştırıldı');
-  next()
-}
+// Template Engine
+
+app.set("view engine", "ejs");
+
+//Middlewares
+
+app.use(express.static('public'));
+
+//Routes
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/about.ejs', (req, res) => {
+  res.render('about');
+});
+
+app.get('/addPhoto.ejs', (req, res) => {
+  res.render('addPhoto');
+});
 
 
-const myLogger2 = (req,res,next)=> {
-  console.log('MiddleWare2 katmanı çalıştırıldı');
-  next()
-}
-//MIDDLEWARES
-app.use(express.static('public'))
-app.use(myLogger)
-app.use(myLogger2)
-
-app.get('/',(req,res) => {
- 
-  const f1Pgoto = {
-    id : 1,
-    name : "Lewis Hamilton",
-    description : "Lewis Hamilton from Mercedes"
-  }
-  res.send(f1Pgoto)
-})
 
 const port = 3000;
-app.listen(port, ()=> {
+app.listen(port, () => {
   console.log(`Uygulama ${port} nolu portu dinlemektedir.`);
-})
+});
