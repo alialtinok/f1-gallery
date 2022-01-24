76c4048c8378bf52cd946045c6f077dccc179243
Ali Altınok <info.alialtinok@gmail.com>
2022-01-24 21:20:30 +0300

    Dependencies and Middleware

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
-const fn = function () {
-  console.log('Heloooo');
-};
+const express = require('express')
+// const res = require('express/lib/response')
+const app = express()
+
+const myLogger = (req,res,next)=> {
+  console.log('MiddleWare katmanı çalıştırıldı');
+  next()
+}
+
+
+const myLogger2 = (req,res,next)=> {
+  console.log('MiddleWare2 katmanı çalıştırıldı');
+  next()
+}
+//MIDDLEWARES
+app.use(express.static('public'))
+app.use(myLogger)
+app.use(myLogger2)
+
+app.get('/',(req,res) => {
+ 
+  const f1Pgoto = {
+    id : 1,
+    name : "Lewis Hamilton",
+    description : "Lewis Hamilton from Mercedes"
+  }
+  res.send(f1Pgoto)
+})
+
+const port = 3000;
+app.listen(port, ()=> {
+  console.log(`Uygulama ${port} nolu portu dinlemektedir.`);
+})
